68bb0e1792449d400f58c84280751b30f4e8a053
Stella Zhang <stellazhang@stella-zhangs-macbook-2.local>
2019-03-01 17:35:31 -0800

    Create search bar with input fields

---
+++ client/src/components/Logo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Logo = props => (
+  <LogoImage>Elite Grub</LogoImage>
+
+);
+
+export default Logo;
+
+const LogoImage = styled.div`
+  font-family: Helvetica Neue,Helvetica,Arial,sans-serif;
+  font-weight: bold;
+  font-size: 1rem;
+`;
--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import Logo from './Logo';
+import Search from './Search';
 
 class Overview extends React.Component {
   constructor(props) {
@@ -7,7 +11,22 @@ class Overview extends React.Component {
 
   render() {
     return (
-      <h1>Elite</h1>
+      <TopHeader>
+        <TopHeaderWrapper>
+          <HeaderContainer>
+            <HeaderItemsContainer>
+              <LogoContainer>
+                <Logo />
+              </LogoContainer>
+              <SearchContainer>
+                <Search />
+              </SearchContainer>
+
+            </HeaderItemsContainer>
+          </HeaderContainer>
+        </TopHeaderWrapper>
+
+      </TopHeader>
     );
   }
 }
@@ -15,3 +34,51 @@ class Overview extends React.Component {
 
 
 export default Overview
+
+const TopHeader = styled.div`
+  min-width: 1020px;
+  z-index: 6;
+`;
+
+const TopHeaderWrapper = styled.div`
+  background: #d32323;
+  padding: 12px 0;
+  position: relative;
+`;
+
+const HeaderContainer = styled.div`
+  width: 960px;
+  margin: 0 auto;
+  padding: 0 15px;
+`;
+
+const HeaderItemsContainer = styled.div`
+  margin-left: -18px;
+  margin-right: -18px;
+  border-collapse: separate;
+  border-spacing: 18px 0;
+  display: table;
+  min-width: 100%;
+  table-layout: auto;
+`;
+
+const LogoContainer = styled.div`
+  margin-left: 9px;
+  margin-right: 9px;
+  border-collapse: collapse;
+  border-spacing: 0 0;
+  vertical-align: middle;
+  box-sizing: border-box;
+  display: table-cell;
+`;
+
+const SearchContainer = styled.div`
+  margin-left: 9px;
+  margin-right: 9px;
+  border-collapse: collapse;
+  border-spacing: 0 0;
+  width: 100%
+  box-sizing: border-box;
+  display: table-cell;
+  vertical-align: middle !important;
+`;
