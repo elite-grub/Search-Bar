8f0e5c5b3bbb1f29810d0c6cb34723333d1436bb
Stella Zhang <39012621+alletsz@users.noreply.github.com>
2019-03-03 00:57:42 -0800

    Revert "Top header"

--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -1,9 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
-
-import Logo from './Logo';
-import Search from './Search';
-import LogAndSign from './LogInSignIn';
 
 class Overview extends React.Component {
   constructor(props) {
@@ -12,27 +7,7 @@ class Overview extends React.Component {
 
   render() {
     return (
-      <TopHeader>
-        <TopHeaderWrapper>
-          <TopRedContainer>
-            <HeaderContainer>
-              <HeaderItemsContainer>
-                <LogoContainer>
-                  <Logo />
-                </LogoContainer>
-                <SearchContainer>
-                  <Search />
-                </SearchContainer>
-                <LogSignUpContainer>
-                  <LogAndSign />
-                </LogSignUpContainer>
-              </HeaderItemsContainer>
-            </HeaderContainer>
-
-          </TopRedContainer>
-        </TopHeaderWrapper>
-
-      </TopHeader>
+      <h1>Elite</h1>
     );
   }
 }
@@ -40,63 +15,3 @@ class Overview extends React.Component {
 
 
 export default Overview
-
-const TopHeader = styled.div`
-  min-width: 1020px;
-  z-index: 6;
-`;
-
-const TopHeaderWrapper = styled.div`
-  background: #d32323;
-  padding: 12px 0;
-  position: relative;
-  border-color: #e6e6e6;
-`;
-
-const TopRedContainer = styled.div`
-  min-width: 1020px;
-  background: none;
-`;
-const HeaderContainer = styled.div`
-  max-width: 960px;
-  margin: 0 auto;
-  padding: 0 15px;
-`;
-
-const HeaderItemsContainer = styled.div`
-  margin-left: -18px;
-  margin-right: -18px;
-  border-collapse: separate;
-  border-spacing: 18px 0;
-`;
-
-const LogoContainer = styled.div`
-  margin-left: 9px;
-  margin-right: 9px;
-  border-collapse: collapse;
-  border-spacing: 0 0;
-  vertical-align: middle;
-  box-sizing: border-box;
-  display: table-cell;
-`;
-
-const SearchContainer = styled.div`
-  margin-left: 9px;
-  margin-right: 9px;
-  border-collapse: collapse;
-  border-spacing: 0 0;
-  width: 100%
-  box-sizing: border-box;
-  display: table-cell;
-  vertical-align: middle !important;
-`;
-
-const LogSignUpContainer = styled.div`
-  margin-left: 9px;
-  margin-right: 9px;
-  border-collapse: collapse;
-  border-spacing: 0 0;
-  vertical-align: middle;
-  box-sizing: border-box;
-  display: table-cell;
-`;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
+// const title = "Search Bar";
+
 ReactDOM.render(<App />, document.getElementById('app'))
